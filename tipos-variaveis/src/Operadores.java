2a7a7b76ba6bfca14cc081a600d9952b5f060342
Juliana Marinho <julianamarinhotecnologia@gmail.com>
2023-04-12 10:47:49 -0300

    .

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,42 @@
+public class Operadores {
+
+    public static void main (String[] args) {
+        
+        double soma = 10.5 + 15.7;
+            System.out.println(soma);
+
+        int subtracao = 113 -25;
+            System.out.println(subtracao);
+
+        int multiplicacao = 20 *7;
+            System.out.println(multiplicacao);
+
+        int divisao = 15 /3;
+            System.out.println(divisao);
+
+        int modulo = 18 % 3;
+            System.out.println(modulo);
+
+        double resultado = (10 * 7) + (20/4);
+            System.out.println(resultado);
+
+
+        int numero = 5;
+
+//Converter número positivo em negativo
+        numero = - numero;
+            System.out.println(numero);
+
+//Converter número negativo em positivo
+        numero = numero * -1 ;
+            System.out.println(numero);
+        
+
+        int j = 6;
+
+//Repetição (numero +1) = numero++
+        j++;
+             System.out.println(j);
+    }
+    
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        double salarioMinimo = 2500;
+            System.out.println(salarioMinimo);
+
+        String nomeCompleto = "JULIANA MARINHO";
+            System.out.println(nomeCompleto);
+
+//Constantes utilizar final 
+        final double parcela = 25.37;
+            System.out.println(parcela);
+
+        char sexo = 'F';
+            System.out.println(sexo);
+
+        boolean doadorOrgao = false;
+            System.out.println(doadorOrgao);
+
+        int idade = 22;
+            System.out.println(idade);
+         
     }
 }
